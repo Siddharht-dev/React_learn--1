1976cbd7a01a2617ffa0832048b609d616c2c633
Siddharht-dev <siddharthwork1719@gmail.com>
2026-01-19 13:38:40 +0530

    fix netify deploy issue

---
+++ 03-props/public/components/Cards.jsx
@@ -0,0 +1,9 @@
+function Cards(props){
+    console.log(props);
+
+    return (
+        <div>Hello this is card</div>
+    );
+}
+
+export default Cards;
---
+++ 03-props/src/App.jsx
@@ -0,0 +1,9 @@
+import Cards from "../public/components/Cards"
+
+export const App = () => {
+  return (
+    <Cards user = "Mahi" id = {18} />
+  )
+}
+
+export default App
